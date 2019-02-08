27c8d181f970a81482a33ce352eb0bcd5440cfc0
abby <awinegar@nmu.edu>
2019-02-08 00:06:47 -0500

    Changed name of roadCoordinate.java to RoadCoordinate.java

--- CatanSrc/RoadCoordinateTest.java
+++ CatanSrc/RoadCoordinateTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class roadCoordinateTest {
+class RoadCoordinateTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
